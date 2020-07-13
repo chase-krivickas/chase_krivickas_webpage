f975cd9ac2483db1d7d590e4ca41966327daf174
ckrivickas <chasekrivickas@Chases-MBP.lan>
2020-07-13 16:49:23 -0600

    deployed webpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up');
+import $ from 'jquery';
+
+import './style.scss';
+
+let count = 0;
+
+setInterval(() => {
+  count += 1;
+  $('#main').html(`You've been on this page for ${count} seconds.`);
+}, 1000);
