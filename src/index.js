72338d8cab271a2c79bb3d6066cf7c407e16ff4c
ckrivickas <chasekrivickas@Chases-MBP.lan>
2020-07-15 11:18:25 -0600

    added react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import $ from 'jquery';
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let count = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-setInterval(() => {
-  count += 1;
-  $('#main').html(`You've been on this page for ${count} seconds.`);
-}, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
